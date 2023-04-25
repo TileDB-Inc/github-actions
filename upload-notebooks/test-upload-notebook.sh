ecd794f74b550d2220ff19dcb90c1f408168ef67
John Blischak <jdblischak@gmail.com>
2023-04-25 16:10:58 -0400

    Change cli args to require passing full remote URIs

--- upload-notebooks/test-upload-notebook.sh
+++ upload-notebooks/test-upload-notebook.sh
@@ -4,19 +4,22 @@ set -u
 echo -e "\n===\nTest: Upload minimal example notebook\n==="
 NOTEBOOK_EXAMPLE="notebook-example-$RANDOM.ipynb"
 cp notebook-example.ipynb $NOTEBOOK_EXAMPLE
-python upload-notebook.py \
-  --tiledb-cloud-namespace=jdblischak \
-  --tiledb-cloud-storage-path s3://tiledb-cloud-jdblischak/notebooks \
-  --tiledb-cloud-storage-credential-name=personal-aws \
-  $NOTEBOOK_EXAMPLE
+python upload-notebooks.py \
+  --notebooks-local $NOTEBOOK_EXAMPLE \
+  --notebooks-remote tiledb://jdblischak/s3://tiledb-cloud-jdblischak/notebooks/${NOTEBOOK_EXAMPLE%.ipynb} \
+  --tiledb-cloud-storage-credential-name=personal-aws
 rm $NOTEBOOK_EXAMPLE
 
 echo -e "\n===\nTest: Fail for missing notebook\n==="
-python upload-notebook.py missing.ipynb
+python upload-notebooks.py \
+  --notebooks-local missing.ipynb \
+  --notebooks-remote tiledb://namespace/s3://missing
 
 echo -e "\n===\nTest: Fail for wrong extension\n==="
 touch wrong-extension.txt
-python upload-notebook.py wrong-extension.txt
+python upload-notebooks.py \
+  --notebooks-local wrong-extension.txt \
+  --notebooks-remote tiledb://namespace/s3://wrong-extension
 rm wrong-extension.txt
 
 echo -e "\n===\nTest: end of tests\n==="
